5dfa5e61618f94148a6129f44f19d0663130b255
Franz04Rony <ventocilla2p@hotmail.com>
2025-09-22 21:11:33 -0500

    hotfix: change to astro 5 and astrojs/vercel 3.0.0

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_DZRFu5G3.mjs';
+import { manifest } from './manifest_DSeEuU4B.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "90d8ae62-9713-44c5-a799-989942a0f432",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
